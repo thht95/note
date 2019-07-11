d4ec00e51187b486160d6b85908fb9c30c61d1e9
Trung Hoa <hoadt9@topica.edu.vn>
2019-07-11 16:43:36 +0700

    2

--- temp2.rb
+++ temp2.rb
@@ -1,84 +1,59 @@
 payload = 
-      {  
-        "customer": { 
-          "group_id":1,
-          "confirmation": 'yes',
-          "created_at": customer.updated_at,
-          "created_in":"Admin",
-          "email": customer.email,
-          "firstname": customer_firstname,
-          "lastname": customer_lastname,
-          "gender": gender[customer.meta_data['gender'].to_s.to_sym],
-          "custom_attributes":[  
-            {  
-                "attribute_code":"role",
-                "value": customer.role
-            },
-            {  
-                "attribute_code":"lang",
-                "value": customer.lang
-            },
-            # {  
-            #     "attribute_code":"headline",
-            #     "value": customer.headline
-            # },
-            {  
-                "attribute_code":"profile_url",
-                "value": customer.profile_url
-            },
-            {  
-                "attribute_code":"uid",
-                "value": customer.id.to_s
-            },
-            {  
-                "attribute_code":"meta_data",
-                "value": customer.meta_data.to_s
-            },
-            {  
-                "attribute_code":"job",
-                "value": customer.job
-            },
-            {  
-                "attribute_code":"links",
-                "value": customer.links.to_s
-            },
-            {  
-                "attribute_code":"unread_notification",
-                "value": customer['unread_notification'].to_s
-            },
-            {  
-                "attribute_code":"seller_teacher_rev_share",
-                "value": customer['seller_teacher_rev_share'].to_s
-            },
-            {  
-                "attribute_code":"seller_topica_rev_share",
-                "value": customer['seller_topica_rev_share'].to_s
-            },
-            {  
-                "attribute_code": "vn_able",
-                "value": customer['vn_able'].to_s
-            },
-            {  
-                "attribute_code": "wishlist",
-                "value": customer.wishlist.to_s
-            },
-            {  
-                "attribute_code": "allot_ratio_instructor",
-                "value": customer['allot_ratio_instructor'].to_s
-            },
-            {  
-                "attribute_code": "allot_ratio_default",
-                "value": customer['allot_ratio_default'].to_s
-            },
-            {
-                "attribute_code": "customer_avatar",
-                "value": customer.avatar
-            },
-            {  
-                "attribute_code": "courses",
-                "value": customer.courses.to_json
-            }
-          ]
+  {  
+    "customer": { 
+      "group_id":1,
+      "confirmation": 'yes',
+      "created_at": customer.updated_at,
+      "created_in":"Admin",
+      "email": customer.email,
+      "firstname": customer_firstname,
+      "lastname": customer_lastname,
+      "gender": gender[customer.meta_data['gender'].to_s.to_sym],
+      "custom_attributes":[  
+        {  
+            "attribute_code":"role",
+            "value": customer.role
+        },
+        {  
+            "attribute_code":"lang",
+            "value": customer.lang
+        },
+        {  
+            "attribute_code":"uid",
+            "value": customer.id.to_s
+        },
+        {  
+            "attribute_code":"meta_data",
+            "value": customer.meta_data.to_s
+        },
+        {
+            "attribute_code": "customer_avatar",
+            "value": customer.avatar
+        },
+        {  
+            "attribute_code": "courses",
+            "value": customer.courses.to_json
         }
-        # "password": "Strong-Password"
-      }
+      ]
+    }
+    # "password": "Strong-Password"
+  }
+
+author = {
+  "username": ENV['MAGENTO_USER_ADMIN'],
+  "password": ENV['MAGENTO_PASSWORD_ADMIN']
+}
+
+access_token = ""
+
+RestClient.post("#{ENV['MAGENTO_HOST']}/integration/admin/token", author.to_json, headers={"Content-Type"=> "application/json"}) { |token, request, result, &block| 
+  access_token = "Bearer"+" "+Hash.from_xml(token)["response"]
+end
+
+if access_token.blank?
+  p "access_token fail"
+else
+  RestClient.post("#{ENV['MAGENTO_HOST']}/customers", payload.to_json, headers={"Authorization" => access_token, "Content-Type": "application/json"}) { |response, request, result, &block| 
+    binding.pry
+  }
+end   
